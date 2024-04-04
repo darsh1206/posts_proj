d74e36be45b68812ddd35f5c0bd2e6cf30281334
darsh1206 <darshpatelahm@gmail.com>
2024-04-04 04:13:41 -0400

    Created the spinner and post template and loading features using Bootstrap

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -1,18 +1,56 @@
 const postsBox = document.getElementById("posts-box");
-$.ajax({
-  type: "GET",
-  url: "/data/",
-  success: function (response) {
-    console.log(response);
-    const data = response.data;
-    console.log(data);
-    data.forEach((element) => {
-      postsBox.innerHTML += `
-            ${element.title} - <b>${element.body}</b><br/>
-        `;
-    });
-  },
-  error: function (error) {
-    console.log(error);
-  },
+const spinnerBox = document.getElementById("spinner-box");
+const loadMoreBtn = document.getElementById("load-btn");
+const endBox = document.getElementById("end-box");
+let visible = 3;
+
+const getData = () => {
+  $.ajax({
+    type: "GET",
+    url: `/data/${visible}/`,
+    success: function (response) {
+      const data = response.data;
+      setTimeout(() => {
+        spinnerBox.classList.add("not-visible");
+        data.forEach((element) => {
+          postsBox.innerHTML += `
+                  <div class="card mb-2">
+                      <div class="card-body" >
+                          <h5 class="card-title">${element.title}</h5>
+                          <p class="card-text">${element.body}</p>    
+                      </div>
+                      <div class="card-footer">
+                            <div class="row">
+                              <div class="col-1">
+                                  <a href="#" class="btn btn-primary ">Details</a>
+                              </div>
+                              <div class="col-1">
+                                  <a href="#" class="btn btn-primary ">Like</a>
+                              </div>
+                            </div>
+                      </div>
+                  </div>
+              `;
+
+          // show posts status
+          if (response.size === 0) {
+            endBox.textContent = "No posts added yet...";
+          } else if (response.size <= visible) {
+            loadMoreBtn.classList.add("not-visible");
+            endBox.textContent = "No more posts to load";
+          }
+        });
+      }, 100);
+    },
+    error: function (error) {
+      console.log(error);
+    },
+  });
+};
+
+loadMoreBtn.addEventListener("click", () => {
+  visible += 3;
+  getData();
 });
+
+getData();
--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from .views import (
     post_list_and_create,
-    load_post_datat_view
+    load_post_data_view
 )
 
 app_name = 'posts'
 
 urlpatterns = [
     path('', post_list_and_create, name = 'main-board'),
-    path('data/',load_post_datat_view, name ='posts-data')
+    path('data/<int:num_posts>/', load_post_data_view, name='posts-data')
 ]
